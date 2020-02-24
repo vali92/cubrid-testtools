9a41ef409470dd9f96b361028292d56d478fd4f5
Neagu Valentin <nv.mihai92@gmail.com>
2020-02-24 14:29:41 +0200

    small update

--- CTP/ha_repl/src/com/navercorp/cubridqa/ha_repl/Test.java
+++ CTP/ha_repl/src/com/navercorp/cubridqa/ha_repl/Test.java
@@ -402,8 +402,6 @@ public class Test {
 	private boolean checkResult(ArrayList<String> actualResultList, String expectResult) {
 		for (String result : actualResultList) {
 			if (!result.equals(expectResult)) {
-				this.userInfo.append("RESULT: " + result).append(Constants.LINE_SEPARATOR);
-				this.userInfo.append("EXPECTED RESULT: " + expectResult).append(Constants.LINE_SEPARATOR);
 				return false;
 			}
 		}
